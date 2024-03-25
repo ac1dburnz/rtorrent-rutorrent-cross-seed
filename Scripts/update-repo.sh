8a4d7a56b147037913e2052e11ee64f26e8a57ad
Ac1dburn <colinborins@hotmail.com>
2024-03-25 16:08:14 -0400

    Create update-repo.sh

---
+++ Scripts/update-repo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+base_dir= os.environ.get("BASE_DIR") 
+
+# Generate a branch name with current date and time
+branch_name="branch_$(date +'%Y%m%d%H%M%S')" 
+
+# Go to directory
+cd "$base_dir/rtorrent-rutorrent-cross-seed"
+
+# Ensure on main branch before creating new one  
+git checkout main
+git pull origin main
+
+# Create and switch to new branch
+git checkout -b "$branch_name"
+
+
+# Run catalog fix script
+python3 "$base_dir/rtorrent-rutorrent-cross-seed/Scripts/rutorrent-auto-update.py"
+
+# Commit changes  
+git add --all :/
+git commit -m "Automatically generated changes on $branch_name"
+
+# Push changes 
+git push origin "$branch_name"
+
+# Create PR
+pr_data='{
+   "title": "Automated update",
+   "body": "Automated PR",
+   "head": "new-branch",
+   "base": "main"  
+}'
+
+pr_response=$(curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "$pr_data" https://api.github.com/repos/$REPO/pulls)
+
+pr_number=$(echo $pr_response | jq '.number')
+
+# Set squash merge
+curl -H "Authorization: token $GITHUB_TOKEN" -X PATCH -d '{"merge_method":"squash"}' https://api.github.com/repos/$REPO/pulls/$pr_number
+
+# Merge PR
+curl -H "Authorization: token $GITHUB_TOKEN" -X PUT https://api.github.com/repos/$REPO/pulls/$pr_number/merge
+
+
+echo "PR merged successfully"
