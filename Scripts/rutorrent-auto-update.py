79e7e7ca8fe65e7fab89c18634e6850ebc35ece5
Ac1dburn <colinborins@hotmail.com>
2024-05-17 16:51:33 -0400

    Update rutorrent-auto-update.py

--- Scripts/rutorrent-auto-update.py
+++ Scripts/rutorrent-auto-update.py
@@ -5,20 +5,16 @@ import os
 repo = "Novik/ruTorrent"
 
 releases_url = f"https://api.github.com/repos/{repo}/releases"
-commits_url = f"https://api.github.com/repos/{repo}/commits"
 
 release_response = requests.get(releases_url)
 latest_release = release_response.json()[0]
 
 tag_name = latest_release["tag_name"] 
 version = re.search(r'v(\d+\.\d+(\.\d+)?)', tag_name).group(1)
-
-commit_response = requests.get(commits_url)
-latest_commit = commit_response.json()[0]
-latest_sha = latest_commit["sha"]
+release_sha = latest_release["target_commitish"]
 
 print(f"Latest version: {version}")
-print(f"Latest commit SHA: {latest_sha}")
+print(f"Release SHA: {release_sha}")
 
 base_dir = os.environ.get("BASE_DIR")
 dockerfile_path = os.path.join(base_dir, "rtorrent-rutorrent-cross-seed", "Dockerfile")
@@ -40,7 +36,7 @@ if comment_line_index is not None:
     lines[comment_line_index] = f'# Novik/ruTorrent {version}\n' 
 
 if arg_line_index is not None:
-    lines[arg_line_index] = f'ARG RUTORRENT_VERSION={latest_sha}\n'
+    lines[arg_line_index] = f'ARG RUTORRENT_VERSION={release_sha}\n'
 
 print("Content of Dockerfile before updating:")
 print("".join(lines))
