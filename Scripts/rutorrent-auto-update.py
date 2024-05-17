2d2552d52353eaf4b56da0a06c3bda0f21e877e7
Ac1dburn <colinborins@hotmail.com>
2024-05-17 16:47:39 -0400

    Update rutorrent-auto-update.py

--- Scripts/rutorrent-auto-update.py
+++ Scripts/rutorrent-auto-update.py
@@ -24,25 +24,33 @@ base_dir = os.environ.get("BASE_DIR")
 dockerfile_path = os.path.join(base_dir, "rtorrent-rutorrent-cross-seed", "Dockerfile")
 
 with open(dockerfile_path, "r") as f:
-  lines = f.readlines()
-  
+    lines = f.readlines()
+    
 comment_line_index = None  
 arg_line_index = None
 
 for i, line in enumerate(lines):
-  if line.startswith("# Novik/ruTorrent"): 
-    comment_line_index = i
-    
-  if line.startswith("ARG RUTORRENT_VERSION="):
-    arg_line_index = i
-    
+    if line.startswith("# Novik/ruTorrent"): 
+        comment_line_index = i
+        
+    if line.startswith("ARG RUTORRENT_VERSION="):
+        arg_line_index = i
+
 if comment_line_index is not None:
-  lines[comment_line_index] = f'# Novik/ruTorrent {version}\n' 
+    lines[comment_line_index] = f'# Novik/ruTorrent {version}\n' 
 
 if arg_line_index is not None:
-  lines[arg_line_index] = f'ARG RUTORRENT_VERSION={latest_sha}\n'
-  
+    lines[arg_line_index] = f'ARG RUTORRENT_VERSION={latest_sha}\n'
+
+print("Content of Dockerfile before updating:")
+print("".join(lines))
+
 with open(dockerfile_path, "w") as f:
-  f.writelines(lines)
-  
+    f.writelines(lines)
+    
+print("Content of Dockerfile after updating:")
+with open(dockerfile_path, "r") as f:
+    print(f.read())
+
 print("Dockerfile updated")
+
