bb6a9d831bc63505eedd9ed1e907a99db4a7d7ab
Ac1dburn <colinborins@hotmail.com>
2024-05-17 16:59:14 -0400

    Update rutorrent-auto-update.py

--- Scripts/rutorrent-auto-update.py
+++ Scripts/rutorrent-auto-update.py
@@ -1,52 +1,53 @@
 import requests
-import re 
+import re
 import os
 
 repo = "Novik/ruTorrent"
 
-releases_url = f"https://api.github.com/repos/{repo}/releases"
-
+# Get the latest release information
+releases_url = f"https://api.github.com/repos/{repo}/releases/latest"
 release_response = requests.get(releases_url)
-latest_release = release_response.json()[0]
+latest_release = release_response.json()
 
-tag_name = latest_release["tag_name"] 
-version = re.search(r'v(\d+\.\d+(\.\d+)?)', tag_name).group(1)
-release_sha = latest_release["target_commitish"]
+# Extract the tag name and version number
+tag_name = latest_release["tag_name"]
+version = re.search(r'v(\d+\.\d+\.\d+)', tag_name).group(1)
+
+# Get the commit SHA associated with the latest release tag
+tags_url = f"https://api.github.com/repos/{repo}/git/refs/tags/{tag_name}"
+tag_response = requests.get(tags_url)
+tag_data = tag_response.json()
+latest_sha = tag_data["object"]["sha"]
 
 print(f"Latest version: {version}")
-print(f"Release SHA: {release_sha}")
+print(f"Latest commit SHA: {latest_sha}")
 
+# Update the Dockerfile
 base_dir = os.environ.get("BASE_DIR")
 dockerfile_path = os.path.join(base_dir, "rtorrent-rutorrent-cross-seed", "Dockerfile")
 
 with open(dockerfile_path, "r") as f:
     lines = f.readlines()
-    
-comment_line_index = None  
+
+comment_line_index = None
 arg_line_index = None
 
 for i, line in enumerate(lines):
-    if line.startswith("# Novik/ruTorrent"): 
+    if line.startswith("# Novik/ruTorrent"):
         comment_line_index = i
-        
+
     if line.startswith("ARG RUTORRENT_VERSION="):
         arg_line_index = i
 
 if comment_line_index is not None:
-    lines[comment_line_index] = f'# Novik/ruTorrent {version}\n' 
+    lines[comment_line_index] = f'# Novik/ruTorrent {version}\n'
 
 if arg_line_index is not None:
-    lines[arg_line_index] = f'ARG RUTORRENT_VERSION={release_sha}\n'
-
-print("Content of Dockerfile before updating:")
-print("".join(lines))
+    lines[arg_line_index] = f'ARG RUTORRENT_VERSION={latest_sha}\n'
 
 with open(dockerfile_path, "w") as f:
     f.writelines(lines)
-    
-print("Content of Dockerfile after updating:")
-with open(dockerfile_path, "r") as f:
-    print(f.read())
 
 print("Dockerfile updated")
+
 
